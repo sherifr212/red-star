3892d209694b0f69dc18863717ba0382c9738d08
Sherif R. <64535505+sherifr212@users.noreply.github.com>
2026-08-19 04:03:10 +0300

    FIX: Add slnx file name to format.

--- src/format-code.ps1
+++ src/format-code.ps1
@@ -2,7 +2,7 @@
 
 
 echo "Running Code Butler (Reorganizing members)..."
-dotnet-code-butler .
+dotnet-code-butler RedStar.slnx
 
 echo "Running dotnet format (Enforcing syntax & spacing)..."
 dotnet format
